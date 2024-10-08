2eed00ef022ef8aef54fcb7675cdbd155b2f3c60
soffiblanco <msblanco@unis.edu.gt>
2024-10-08 17:59:07 -0600

    Product catalog
    product catalog structure with api test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
 import CardOriginal from './components/Card';
 import NavBarReal from './layouts/header/NavBar';
 
+=======
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './layouts/Header/NavBar';
+import LoginForm from '././components/loginComponent/LoginForm'
+import ItemCatalog from './components/Catalog/ItemCatalog';
+>>>>>>> Stashed changes
 
 function App() {
   return (
