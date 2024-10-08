20a381fe621abcf91c3804c2b033bc6cfc6ba149
KRSagastumE04 <125618133+KRSagastumE04@users.noreply.github.com>
2024-10-08 17:19:23 -0600

    Integration of Backend and Frontend for User Registration and Login
    Integrated backend and frontend for registration and login. Updated icons and improved design

--- src/components/loginComponent/LoginForm.jsx
+++ src/components/loginComponent/LoginForm.jsx
@@ -272,11 +272,10 @@ const LoginForm = () => {
 };
 
 export default LoginForm; */
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import './LoginForm.css';
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock,FaEnvelope,FaPhone,FaMapMarkerAlt,FaCcVisa,FaImage } from "react-icons/fa";
 
 const LoginForm = () => {
     const [isLoginActive, setIsLoginActive] = useState(true);
@@ -286,11 +285,17 @@ const LoginForm = () => {
         email: '',
         password: '',
         direction: '',
-        telephoneNumber: '',
-        cardNumber: '',
-        expirationDate: '',
+        telephone_number: '',
+        card_number: '',
+        expiration_date: '',
         cvv: '',
     });
+
+
+
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false); // Para manejar el estado de carga
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -299,174 +304,223 @@ const LoginForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const endpoint = isLoginActive ? '/api/login' : '/api/register';
-        
-
+        const endpoint = 'http://localhost/apis/users.php';
         const data = isLoginActive
-            ? { username: formData.username, password: formData.password }
+            ? { action: 'login', email: formData.email, password: formData.password }
             : { 
+                action: 'register',
                 name: formData.name, 
                 email: formData.email, 
                 password: formData.password, 
                 direction: formData.direction, 
-                telephoneNumber: formData.telephoneNumber, 
-                cardNumber: formData.cardNumber, 
-                expirationDate: formData.expirationDate, 
-                cvv: formData.cvv 
+                telephone_number: formData.telephone_number, 
+                user_image: formData.user_image,
+                role: formData.role,
+                card_number: formData.card_number, 
+                expiration_date: formData.expiration_date, 
+                cvv: formData.cvv,
             };
 
+        console.log('Datos enviados:', data);
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+
+        setLoading(true); // Inicia el estado de carga
         try {
-            const response = await axios.post(endpoint, data);
-            console.log(response.data);
+            const response = await axios.post(endpoint, data, config);
+        
+            if (response.status === 200) {
+                console.log('Respuesta del servidor:', response.data);
+                if (response.data.success) {
+                    window.alert(response.data.message); // Muestra mensaje de éxito
+
+                } else {
+                    window.alert(response.data.message); // Muestra el mensaje de error del servidor
+                }
+            } else {
+                window.alert('Algo salió mal en el servidor.'); // Muestra mensaje de error genérico
+            }
         } catch (error) {
-            console.error(`${isLoginActive ? 'Login' : 'Registration'} failed:`, error);
+            console.error('Error en la solicitud:', error.response ? error.response.data : error);
+            window.alert(error.response?.data?.message || 'Error en la solicitud.'); // Muestra el mensaje de error
+        } finally {
+            setLoading(false); // Finaliza el estado de carga
         }
     };
 
+    const showMessage = (text, type = 'success') => (
+        <div className={`message ${type}`}>
+            <p>{text}</p>
+        </div>
+    );
+
     return (
         <div className={`wrapper ${isLoginActive ? 'login-active' : 'register-active'}`}>
-                <form onSubmit={handleSubmit}>
-                    <h1>{isLoginActive ? 'Login' : 'Registration'}</h1>
+        
+            <form onSubmit={handleSubmit}>
+                <h1>{isLoginActive ? 'Login' : 'Registration'}</h1>
 
+                {isLoginActive ? (
+                    <>
+                        <div className="input-box">
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder='Email'
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                            />
+                            <FaEnvelope className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder='Password'
+                                value={formData.password}
+                                onChange={handleChange}
+                                required
+                            />
+                            <FaLock className='icon' />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder='Name'
+                                value={formData.name}
+                                onChange={handleChange}
+                                required
+                            />
+                            <FaUser className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder='Email'
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                            />
+                            <FaEnvelope className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder='Password'
+                                value={formData.password}
+                                onChange={handleChange}
+                                required
+                            />
+                            <FaLock className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                name="direction"
+                                placeholder='Direction'
+                                value={formData.direction}
+                                onChange={handleChange}
+                            />
+                            <FaMapMarkerAlt className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="tel"
+                                name="telephone_number"
+                                placeholder='Telephone Number'
+                                value={formData.telephone_number}
+                                onChange={handleChange}
+                            />
+                            <FaPhone  className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="file"
+                                name="user_image"
+                                placeholder='User_Image'
+                                value={formData.user_image}
+                                onChange={handleChange}
+                            />
+                            <FaImage className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                name="role"
+                                placeholder='Role'
+                                value={formData.role}
+                                onChange={handleChange}
+                                required
+                            />
+                            <FaUser className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                name="card_number"
+                                placeholder='Card Number'
+                                value={formData.card_number}
+                                onChange={handleChange}
+                            />
+                            <FaCcVisa className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                name="expiration_date"
+                                placeholder='Expiration Date (YY-MM-DD)'
+                                value={formData.expiration_date}
+                                onChange={handleChange}
+                            />
+                            <FaCcVisa className='icon' />
+                        </div>
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                name="cvv"
+                                placeholder='CVV'
+                                value={formData.cvv}
+                                onChange={handleChange}
+                            />
+                            <FaCcVisa className='icon' />
+                        </div>
+                    </>
+                )}
+
+                <div className="remember-forgot">
                     {isLoginActive ? (
                         <>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    placeholder='Username'
-                                    value={formData.username}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    placeholder='Password'
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaLock className='icon' />
-                            </div>
+                            <a href="#">Forgot password?</a>
                         </>
                     ) : (
-                        <>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="name"
-                                    placeholder='Name'
-                                    value={formData.name}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="email"
-                                    placeholder='Email'
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    placeholder='Password'
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaLock className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="direction"
-                                    placeholder='Direction'
-                                    value={formData.direction}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="telephoneNumber"
-                                    placeholder='Telephone Number'
-                                    value={formData.telephoneNumber}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="cardNumber"
-                                    placeholder='Card Number'
-                                    value={formData.cardNumber}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="expirationDate"
-                                    placeholder='Expiration Date'
-                                    value={formData.expirationDate}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                            <div className="input-box">
-                                <input
-                                    type="text"
-                                    name="cvv"
-                                    placeholder='CVV'
-                                    value={formData.cvv}
-                                    onChange={handleChange}
-                                    required
-                                />
-                                <FaUser className='icon' />
-                            </div>
-                        </>
+                        <label></label>
                     )}
-
-                    <div className="remember-forgot">
-                        {isLoginActive ? (
-                            <>
-                                <label><input type="checkbox" /> Remember me</label>
-                                <a href="#">Forgot password?</a>
-                            </>
-                        ) : (
-                            <label><input type="checkbox" /> I agree to the terms & conditions</label>
-                        )}
-                    </div>
-                    <button type="submit">{isLoginActive ? 'Login' : 'Register'}</button>
-                    <div className="register-link">
-                        <p>
-                            {isLoginActive ? "Don't have an account?" : "Already have an account?"}
-                            <a href="#" onClick={() => setIsLoginActive(!isLoginActive)}>
-                                {isLoginActive ? 'Register' : 'Login'}
-                            </a>
-                        </p>
-                    </div>
-                </form>
-            
+                </div>
+                
+                <button type="submit" disabled={loading}>
+                    {loading ? 'Loading...' : (isLoginActive ? 'Login' : 'Register')}
+                </button>
+                
+                <div className="register-link">
+                    <p>
+                        {isLoginActive ? "Don't have an account?" : "Already have an account?"}
+                        <a href="#" onClick={() => setIsLoginActive(!isLoginActive)}>
+                            {isLoginActive ? 'Register' : 'Login'}
+                        </a>
+                    </p>
+                </div>
+            </form>
         </div>
     );
 };
