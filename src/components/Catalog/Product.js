1d49649d38c110d756264b1589d1f614e90d3a30
soffiblanco <msblanco@unis.edu.gt>
2024-10-08 18:02:04 -0600

    Revert "Product catalog"
    This reverts commit 2eed00ef022ef8aef54fcb7675cdbd155b2f3c60.

--- src/components/Catalog/Product.js
+++
@@ -1,7 +0,0 @@
-import React, {useContext} from 'react';
-
-const Product = () => {
-    return <div>Product</div>
-};
-
-export default Product;
--- src/layouts/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Footer from "../footer/FooterStyles";
-
-
-const Footer = () => {
-    return <div>Footer</div>
-}
-
-export default Footer;
